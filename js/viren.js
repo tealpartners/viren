3c558c702d80011bf4896d845d54cb2321f28e81
Toon Struyf <struyf@gmail.com>
2018-06-05 18:07:59 +0200

    proper scaling attemp1

--- js/viren.js
+++ js/viren.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
+    rescale();
+})
 
+function rescale(){
     vp_width = document.body.clientWidth;
     margin = 20;
     object = 800;
@@ -11,9 +14,20 @@ $(document).ready(function(){
         scale = scale_max;
     }
 
+    if (vp_width > 600 && vp_width < 900){
+        
+    }
+
+    if (vp_width > 900 && vp_width < 1100){
+        if(scale > 0.48){
+            scale = 0.48;
+        }
+    }
+
     scale_str = 'scale(' + scale + ')';
-
-
     $('.js-window').css('transform', scale_str );
+}
 
-})
+$( window ).resize(function() {
+    rescale();
+});
